cbacff8b022056507080d2e6e99daab049b62fed
Luis <lmartinez.marina@atsistemas.com>
2016-04-12 10:49:25 +0200

    update test structure

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,12 +4,12 @@ import Logo from '../logo'
 import Home from '../../pages/home'
 
 class App extends Component {
-
   render() {
     return (
       <Router>
         <Route path="/" component={Logo} />
         <Route path="/home" component={Home} />
+        <Route path="*" component={Home} />
       </Router>
     );
   }
