8115561c853349cb4b4515c512fc8f632b1646bd
Pablo Magaz <pmagaz@atsistemas.com>
2016-04-15 20:29:12 +0200

    Update configuration

--- src/conf/.react-base/logo.js
+++ src/conf/.react-base/logo.js
@@ -1,5 +1,5 @@
 'use strict';
 
 var fs = require('fs');
-var dir = process.cwd()+'/.react-base/conf/logo.txt';
+var dir = process.cwd()+'/src/conf/.react-base/logo.txt';
 console.log(fs.readFileSync(dir, 'utf-8'));
