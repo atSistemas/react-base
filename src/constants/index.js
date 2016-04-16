1b13907fd8ab9235d61ab7b0f3f290da3500cd77
Pablo Magaz <pmagaz@atsistemas.com>
2016-04-16 13:25:05 +0200

    Fix constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,5 @@
-export const ADD_ITEM = 'ADD_ITEM'
+/*export const ADD_ITEM = 'ADD_ITEM'
 export const EDIT_ITEM = 'EDIT_ITEM'
-export const REMOVE_ITEM = 'REMOVE_ITEM'
+export const REMOVE_ITEM = 'REMOVE_ITEM'*/
+export const FETCH_ITEMS = 'FETCH_ITEMS'
+export const RECEIVE_ITEMS = 'RECEIVE_ITEMS'
