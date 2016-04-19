4eed9dbe28afe9106ae13e29ae66b8d817f6bb3f
Pablo Magaz <pmagaz@atsistemas.com>
2016-04-19 12:54:45 +0200

    Change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   devtool: 'inline-source-map',
@@ -16,9 +18,18 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /.jsx?$/, loader: 'babel-loader', exclude: /node_modules/, query: { presets: ['es2015', 'react']
-        }
+      { test: /.js?$/, loader: 'babel-loader', exclude: /node_modules/, query: { presets: ['es2015', 'react']}},
+      {
+        test: /\.css$/i,
+        loader: ExtractTextPlugin.extract('style',
+          `css?modules&localIdentName=[name]_[local]__[hash:base64:5]!postcss`),
       }
     ]
   },
+  postcss: [
+    autoprefixer({ browsers: ['last 2 versions'] })
+  ],
+  plugins: [
+    new ExtractTextPlugin('style.css', { allChunks: true })
+  ]
 };
