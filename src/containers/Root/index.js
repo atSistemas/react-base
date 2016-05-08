4f9a9655c715071009edff0f2ec6ec9ac8190341
Pablo Magaz <pmagaz@atsistemas.com>
2016-05-08 22:03:20 +0200

    Remove unncesary import

--- src/containers/Root/index.js
+++ src/containers/Root/index.js
@@ -1,6 +1,5 @@
 import { Provider } from 'react-redux'
 import React, { Component } from 'react'
-import { syncHistoryWithStore } from 'react-router-redux'
 import { Router, browserHistory } from 'react-router'
 
 import routes from '../../routes'
@@ -8,7 +7,6 @@ import configureStore from '../../store/configure-store'
 
 let state = window.$REACTBASE_STATE
 const store = configureStore(state)
-const history = syncHistoryWithStore(browserHistory, store)
 
 class Root extends Component {
 
