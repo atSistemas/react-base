e1743319031fd439467dc021d6008d473e2911bb
Luis <lmartinez.marina@atsistemas.com>
2016-05-17 10:18:17 +0200

    update webpack chunk common file

--- webpack/webpack-prod-config.js
+++ webpack/webpack-prod-config.js
@@ -1,15 +1,24 @@
 import path from 'path'
 import webpack from 'webpack'
-import CopyWebpackPlugin from 'copy-webpack-plugin'
+import copyWebpackPlugin from 'copy-webpack-plugin'
 
 const clientPath = path.resolve(__dirname, '..', 'src', 'client.js')
 
 export const prodPlugins = [
   new webpack.optimize.OccurenceOrderPlugin(),
   new webpack.optimize.UglifyJsPlugin({compressor: { warnings: false }}),
-  new CopyWebpackPlugin([{ from: './src/assets', to: 'assets' }])
+  new webpack.optimize.CommonsChunkPlugin('common', 'common.js'), 
+  new copyWebpackPlugin([{ from: './src/assets', to: 'assets' }])
 ]
 
-export const prodEntries = [
-  clientPath
-]
+export const prodEntries = {
+  app: clientPath,
+  common: [ 
+    'isomorphic-fetch', 
+    'react', 
+    'react-dom', 
+    'react-redux', 
+    'react-router', 
+    'redux'
+  ]
+}
