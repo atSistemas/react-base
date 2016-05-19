e2d99cbda42e1993f667cbf09e9c20b6f325cdfe
Rafael Bernad de Castro <rbernad@atsistemas.com>
2016-05-19 10:26:59 +0200

    Create an inmutable list with server received data; refactor changeItemState

--- src/reducers/Items.js
+++ src/reducers/Items.js
@@ -1,29 +1,24 @@
 import types from '../types'
 import createReducer from '../shared/create-reducer';
-
-const initialState = []
+import Immutable from 'immutable'
 
 function itemsRequest( state ){ return state }
 
 function itemsError( state ){ return state }
 
 function itemsSucess(state, action) {
-  return { ...state, data: action.result }
+  return Immutable.List(action.result);
 }
 
 function changeItemState(state, action){
-  let items = []
-  state.data.map(function (item) {
+  let items = state.map((item) => {
     if (item.id === action.id) {
-      item.removed = !item.removed
+      item.removed = !item.removed;
     }
-    items.push(item)
-  })
-
-  return { ...state, data: items }
+    return item;
+  });
+  return items;
 }
-
-
 
 const handlers = {
   [types.ITEMS_REQUEST]: itemsRequest,
@@ -32,4 +27,4 @@ const handlers = {
   [types.CHANGE_ITEM_STATE]: changeItemState
 }
 
-export default createReducer( initialState, handlers );
+export default createReducer(handlers, []);
