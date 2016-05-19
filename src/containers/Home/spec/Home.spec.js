07a460f35460f0cd7ace59b008e966042658aad6
Luis <lmartinez.marina@atsistemas.com>
2016-05-19 10:23:06 +0200

    added test components home, app

---
+++ src/containers/Home/spec/Home.spec.js
@@ -0,0 +1,33 @@
+import expect from 'expect'
+import React from 'react'
+import TestUtils from 'react-addons-test-utils'
+import Home from '..'
+
+function setup() {
+  let props = {
+    
+  }
+
+  let renderer = TestUtils.createRenderer()
+  renderer.render(<Home {...props} />)
+  let output = renderer.getRenderOutput()
+
+  return {
+    props,
+    output,
+    renderer
+  }
+}
+
+describe('components', () => {
+  describe('Home', () => {
+    it('should render correctly', () => {
+      const { output } = setup()
+
+      expect(output.type).toBe('div')
+
+      const [ div , linkSection] = output.props.children
+      expect(linkSection.type.displayName).toEqual('Link')
+    })
+  })
+})
