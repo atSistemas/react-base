59c69c0222671fdbdf9bb82c36310862a2d17a85
Rafael Bernad de Castro <rbernad@atsistemas.com>
2016-05-19 10:25:13 +0200

    Create reducers with inmutable initial states

--- src/shared/create-reducer.js
+++ src/shared/create-reducer.js
@@ -1,6 +1,6 @@
 import Immutable from 'immutable'
 
-export default function createReducer (actionHandlers, initialState = window.$REACTBASE_STATE || {}) {
+export default function createReducer (actionHandlers, initialState = window ? window.$REACTBASE_STATE : {}) {
 
   return (state = Immutable.fromJS(initialState), action) => {
     const reduceFn = actionHandlers[action.type];
