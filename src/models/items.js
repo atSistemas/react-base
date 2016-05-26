013be66b1148373a12412f9878ac19d652b0fbcc
Pablo Magaz <pmagaz@atsistemas.com>
2016-05-26 11:36:43 +0200

    Change model record

--- src/models/items.js
+++ src/models/items.js
@@ -1,15 +1,10 @@
 import { Record, Map } from 'immutable';
 
-class Item {
+export const itemModel = new Record({
+	id: 0,
+	userId: 0,
+	title: '',
+	body: ''
+});
 
-	constructor() {
-		this.userId = 0;
-		this.id = 0;
-		this.title = null;
-		this.body = null;
-	}
-
-}
-
-export const itemModel = new Record( new Item() );
 export const itemCollection = new Record({ data: new Map() });
