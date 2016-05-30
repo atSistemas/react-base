6d75fd87d7f227acd5b68ba83adcc8fa05441547
Pablo Magaz <pmagaz@atsistemas.com>
2016-05-30 11:55:05 +0200

    Add context for postcss support

--- webpack/webpack-prod-config.js
+++ webpack/webpack-prod-config.js
@@ -4,7 +4,9 @@ import copyWebpackPlugin from 'copy-webpack-plugin';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 const mainPath = path.resolve(__dirname, '..');
-const clientPath = path.resolve(__dirname, '..', 'src', 'client/');
+const clientPath = path.resolve(__dirname, '..', 'src','client/');
+
+export const prodContext = path.resolve(__dirname, '../src');
 
 export const prodPlugins = [
   new webpack.optimize.OccurenceOrderPlugin(),
@@ -27,7 +29,6 @@ export const prodEntries = {
 };
 
 export const prodLoaders = [
-  { test: [/\.js$/, /\.jsx$/], loader: 'babel', exclude: /node_modules/, include: mainPath, query: { presets: ["es2015", "stage-0", "react"] } },
-  { test: /\.css/, loader: ExtractTextPlugin.extract('style-loader',  'css-loader?module','&localIdentName=[name]__[local]___[hash:base64:5]','&disableStructuralMinification','!postcss-loader')}
-
-];
+  { test: [/\.js$/, /\.jsx$/],loader: 'babel',exclude: /node_modules/,include: mainPath, query: { presets: ["react-hmre", "es2015", "stage-0", "react"] }},
+  { test: /\.css/, loader: ExtractTextPlugin.extract('style',  'css?modules&importLoaders=1&localIdentName=[name]__[local]-[hash:base64:4]!postcss')}
+];;
