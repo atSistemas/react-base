129435c5ebcab9f034f7856e59ea438d483a9bdd
Pablo Magaz <pmagaz@atsistemas.com>
2016-06-06 17:43:44 +0200

    Change import order to handle correctly webpack build

--- server/server.js
+++ server/server.js
@@ -3,15 +3,9 @@ import { match } from 'react-router';
 import { applyMiddleware, createStore } from 'redux';
 
 import statics from './statics';
-import routes from '../src/routes';
 import ENV from '../src/shared/env';
-import rootReducer from '../src/reducers/';
+import { symbols, color } from './shared/console';
 import applyEnvMiddleWare from './middleware';
-import {symbols, color} from './shared/console';
-import renderMainPage from './templates/main-page';
-import requestMiddleware from '../src/middleware/request';
-import fetchRequiredActions from '../src/shared/fetch-data';
-import renderMainContainer from './containers/main-container';
 
 const port = 8000;
 const app = express();
@@ -19,6 +13,13 @@ const context = 'server';
 const staticPaths = setStaticsPaths(statics);
 const envMiddleware = applyEnvMiddleWare(ENV, app);
 const serverStore = applyMiddleware( requestMiddleware )( createStore );
+
+import routes from '../src/routes';
+import rootReducer from '../src/reducers/';
+import renderMainPage from './templates/main-page';
+import requestMiddleware from '../src/middleware/request';
+import fetchRequiredActions from '../src/shared/fetch-data';
+import renderMainContainer from './containers/main-container';
 
 app.use(function (req, res) {
 
