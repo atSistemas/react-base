4c52823d05a80e0dccf742353895461fd7436602
Pablo Magaz <pmagaz@atsistemas.com>
2016-06-08 13:00:01 +0200

    Add routes generator

--- app/shared/Generate.js
+++ app/shared/Generate.js
@@ -1,12 +1,17 @@
 require('babel-core/register');
 const args = process.argv.slice(2);
+const generateModelIndex = require('./generators/GenerateModelIndex.js');
+const generateReducerIndex = require('./generators/GenerateReducerIndex.js');
+const generateRoutes = require('./generators/GenerateRoutes.js');
 
-if(~args.indexOf('--model')){
-  const generateModelIndex = require('./generators/GenerateModelIndex.js');
+if(~args.indexOf('--all')  || ~args.indexOf('--routes')){
+  generateRoutes();
+}
+
+if(~args.indexOf('--all') || ~args.indexOf('--model')){
   generateModelIndex();
 }
 
-if(~args.indexOf('--reducer')){
-  const generateReducerIndex = require('./generators/GenerateReducerIndex.js');
+if(~args.indexOf('--all')  || ~args.indexOf('--reducer')){
   generateReducerIndex();
 }
---
+++ app/shared/generators/GenerateRoutes.js
@@ -0,0 +1,54 @@
+import path from 'path';
+import { symbols, color } from '../../../server/shared/console';
+import { fileExists, readDir, writeFile } from '../FileSystem';
+
+const containersPath = path.resolve(__dirname, '..', '..', 'containers');
+const routerPath = path.resolve(__dirname, '..', '..', 'routes','index.js');
+
+let routesImports = 'import React from \'react\';\nimport { Route, IndexRoute } from \'react-router\';\n\nimport App from \'containers/App/\'';
+
+function GenerateRoutes(){
+  const routes = getRoutes();
+  let routesExports = '';
+  let newRoutes = '';
+
+  routes.forEach(function(route, index){
+    if(route.import){
+      routesImports += (index === 0) ? route.import : '\n' + route.import;
+      newRoutes += '    <Route path="/' + route.name.toLowerCase() + '" component={ _' + route.name +' } />\n';
+    }
+  });
+
+  newRoutes = generateRoutes(newRoutes)
+  routesExports = generateRoutesExport(routesExports);
+  let content = routesImports + newRoutes + routesExports;
+  let result = writeFile(routerPath, content);
+  if(result){
+    console.log('[BASE] ' + color('success', symbols.ok) + ' Routes generated correctly!');
+  } else {
+    console.log('[BASE] ' + color('error', symbols.err) + ' ' + result);
+  }
+}
+
+function generateRoutes(newRoutes){
+  return '\n\nconst routes = (\n  <Route path="/" component={ App } >\n    <IndexRoute component={ _Main } />\n  ' + newRoutes + '  </Route>\n);\n';
+}
+
+
+function generateRoutesExport(routesExports){
+  return '\nexport default routes;';
+}
+
+function getRoutes(){
+  const files = readDir(containersPath);
+  return files.map(function(container){
+    let containerPath = path.resolve(containersPath, container);
+    if(fileExists(containerPath) && container !== 'App'){
+      return { name:container, import:'import _' + container + ' from \'containers/'+ container +'/\';' };
+    } else {
+      return { name: container, import: null };
+    }
+  });
+}
+
+module.exports = GenerateRoutes;
