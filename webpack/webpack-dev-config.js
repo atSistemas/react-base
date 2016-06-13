3377c5ebaacd17061710d00b5d6a7b9f49ddb6cf
Pablo Magaz <pmagaz@atsistemas.com>
2016-06-13 13:34:39 +0200

    Change babel to babel-loader

--- webpack/webpack-dev-config.js
+++ webpack/webpack-dev-config.js
@@ -1,7 +1,7 @@
 import path from 'path';
 import webpack from 'webpack';
-import AssetsPlugin from 'assets-webpack-plugin';
-import ExtractTextPlugin from 'extract-text-webpack-plugin'
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
 const mainPath = path.resolve(__dirname, '..');
 const clientPath = path.resolve(__dirname, '..', 'app', 'client/');
 
@@ -29,6 +29,6 @@ export const devEntries = [
 ];
 
 export const devLoaders = [
-  { test: [/\.js$/, /\.jsx$/],loader: 'babel',exclude: /node_modules/,include: mainPath, query: { presets: ["react-hmre", "es2015", "stage-0", "react"] }},
+  { test: [/\.js$/, /\.jsx$/],loader: 'babel-loader',exclude: /node_modules/,include: mainPath, query: { presets: ["react-hmre", "es2015", "stage-0", "react"] }},
   { test: /\.css/, loader: ExtractTextPlugin.extract('style-loader',  'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]-[hash:base64:4]!postcss-loader')}
 ];
