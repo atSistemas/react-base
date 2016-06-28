3cebf2285d752f7f98a0745553c62b77b3545cea
Rafael Bernad de Castro <rbernad@atsistemas.com>
2016-06-28 10:14:29 +0200

    add development server (start method)

---
+++ .react-base/start.js
@@ -0,0 +1,31 @@
+//"start": "npm run logo && cross-env NODE_ENV=development node server/",
+require('babel-core/register');
+
+import consoleHelper from './console-helper';
+import childProcess from 'child_process';
+
+const start = () => {
+
+  let process = childProcess.fork('./server/index.js');
+
+  // listen for errors as they may prevent the exit event from firing
+  process.on('error', function (err) {
+    console.log(consoleHelper.error("Error launching development environment:"));
+    console.error(err);
+    process.exit(0);
+  });
+
+  process.on('exit', function (code) {
+    let err = code === 0 ? null : new Error('exit code ' + code);
+    if (err) {
+      console.log(consoleHelper.error("Error launching development environment:"));
+      console.error(err);
+    } else {
+      console.log(consoleHelper.line("Development server terminated"));
+    }
+  });
+
+};
+
+let args = process.argv.slice(2);
+require.main === module ? start(...args) : module.exports = start;
