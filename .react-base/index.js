c5294b865ce8061250fbcbabc2a32457a9d85219
Rafael Bernad de Castro <rbernad@atsistemas.com>
2016-06-28 10:13:42 +0200

    Parametrize build method; add start method

--- .react-base/index.js
+++ .react-base/index.js
@@ -3,13 +3,15 @@
 'use strict';
 require('babel-core/register');
 
+const consoleHelper = require('./console-helper');
 const logo = require('./logo');
 const methods = require('./methods');
 const baseConsole = require('./console-helper');
 const test = require('./test');
 const build = require('./build');
+const start = require('./start');
 
-const index = (method) => {
+const index = (method, param1) => {
 
   baseConsole.clear();
 
@@ -25,12 +27,29 @@ const index = (method) => {
     case methods.LINT:
       break;
     case methods.BUILD:
+      if (param1) {
+        if (param1) {
+          console.log(consoleHelper.line(`[BASE] Building project using ${param1.toUpperCase()} environment`));
+          process.env['NODE_ENV'] = param1;
+        }
+      } else {
+        process.env['NODE_ENV'] = 'production';
+      }
       build();
       break;
     case methods.TEST:
       test();
       break;
     case methods.START:
+      if (param1) {
+        if (param1) {
+          console.log(consoleHelper.line(`[BASE] Starting development server using ${param1.toUpperCase()} environment`));
+          process.env['NODE_ENV'] = param1;
+        }
+      } else {
+        process.env['NODE_ENV'] = 'development';
+      }
+      start();
       break;
     default:
   }
