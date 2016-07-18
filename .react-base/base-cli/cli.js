3803b07dfac1759cd3b1ba56597f3fe5e614193f
Rafael Bernad de Castro <rbernad@atsistemas.com>
2016-07-18 14:33:43 +0200

    fix build issue

---
+++ .react-base/base-cli/cli.js
@@ -0,0 +1,22 @@
+import fs from 'fs';
+import path from 'path';
+import minimist from 'minimist';
+import logo from './logo';
+import consoleHelper from './helpers/console';
+
+const argv = minimist(process.argv.slice(2));
+const subcommand = argv._[0].toLowerCase();
+
+consoleHelper.clear();
+logo();
+
+fs.exists(path.join(__dirname, 'methods', `${subcommand}.js`), (exists) => {
+  if (exists) {
+    argv._ = argv._.slice(1);
+    require(`./methods/${subcommand}`)(argv);
+  } else {
+    if (subcommand !== 'logo') {
+      console.log(consoleHelper.error(`Unknown command: ${subcommand}`));
+    }
+  }
+});
