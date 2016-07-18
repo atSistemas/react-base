c9506cf989741726b94457ba75daffb7643d40b2
Rafael Bernad de Castro <rbernad@atsistemas.com>
2016-07-18 13:38:18 +0200

    convert to ES6 imports; move to helpers

---
+++ .react-base/base-cli/helpers/logo.js
@@ -0,0 +1,10 @@
+import fs from 'fs';
+import path from 'path';
+
+const dir = path.join(__dirname + '../assets/logo.txt');
+
+const logo = () => {
+  console.log(fs.readFileSync(dir, 'utf-8'));
+};
+
+require.main === module ? logo() : module.exports = logo;
