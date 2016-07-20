ebf11c678237f3b8eb459f50cfac6947777f8b62
Rafael Bernad de Castro <rbernad@atsistemas.com>
2016-07-20 10:17:15 +0200

    streamline arguments processing

--- .react-base/base-cli/methods/lint.js
+++ .react-base/base-cli/methods/lint.js
@@ -1,19 +1,20 @@
 import lintSrc from './lint-code';
 import lintCss from './lint-styles';
+import minimist from 'minimist';
 
 const lint = (args) => {
 
-    let lintType = args._.length ? args._[0].toLowerCase() : undefined;
+  let lintType = args ? args.toLowerCase() : undefined;
 
-    if (!lintType) {
-        lintSrc();
-        lintCss();
-    } else if (lintType === 'code') {
-        lintSrc();
-    } else if (lintType === 'styles') {
-        lintCss();
-    }
+  if (!lintType) {
+    lintSrc();
+    lintCss();
+  } else if (lintType === 'js' || lintType === 'javascript') {
+    lintSrc();
+  } else if (lintType === 'styles') {
+    lintCss();
+  }
 };
 
-let args = require('minimist')(process.argv.slice(2));
-require.main === module ? lint(args) : module.exports = lint;
+let args = minimist(process.argv.slice(2));
+require.main === module ? lint(args._.slice(1)) : module.exports = lint;
