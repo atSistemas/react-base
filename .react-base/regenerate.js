bfb64d862503a3ab747dba582df325a1617c8619
Rafael Bernad de Castro <rbernad@atsistemas.com>
2016-07-20 13:06:39 +0200

    move regeneration scripts to .react-base

--- .react-base/regenerate.js
+++ .react-base/regenerate.js
@@ -4,20 +4,20 @@ const modelIndex = require('./regenerators/RegenerateModelIndex.js');
 const reducerIndex = require('./regenerators/RegenerateReducerIndex.js');
 const regenerateRoutes = require('./regenerators/RegenerateRoutes.js');
 
-const modelFilePath = path.resolve(__dirname, '..', 'models','index.js');
-const reducerFilePath = path.resolve(__dirname, '..', 'reducers','index.js');
+const modelFilePath = path.resolve(process.cwd(), 'app', 'models','index.js');
+const reducerFilePath = path.resolve(process.cwd(), 'app', 'reducers', 'index.js');
+const containersPath = path.resolve(process.cwd(), 'app', 'containers');
 
-const containersPath = path.resolve(__dirname, '..', 'containers');
 const args = process.argv.slice(2);
 
-if(~args.indexOf('--all')  || ~args.indexOf('--routes')){
+if(~args.indexOf('--all')  || ~args.indexOf('--routes') || !args){
   regenerateRoutes();
 }
 
-if(~args.indexOf('--all') || ~args.indexOf('--model')){
+if(~args.indexOf('--all') || ~args.indexOf('--model') || !args){
   modelIndex.RegenerateModelIndex(containersPath, modelFilePath);
 }
 
-if(~args.indexOf('--all')  || ~args.indexOf('--reducer')){
+if(~args.indexOf('--all')  || ~args.indexOf('--reducer') || !args){
   reducerIndex.RegenerateReducerIndex(containersPath, reducerFilePath);
 }
