ed0106a78529e2db1f688b9e93d04979648a8657
Rafael Bernad de Castro <rbernad@atsistemas.com>
2016-07-20 13:17:11 +0200

    add generator method to CLI

---
+++ .react-base/base-cli/methods/generate.js
@@ -0,0 +1,23 @@
+import consoleHelper from '../helpers/console';
+import scriptRunner from '../helpers/script-runner';
+import minimist from 'minimist';
+import yeomanEnv from 'yeoman-generator';
+
+const generate = () => {
+
+  const generator = yeomanEnv().create('./node_modules/generator-react-base');
+  generator.destinationRoot(process.cwd());
+  generator.run((err) => {
+    if (err) {
+      console.log(consoleHelper.error("Component generation failed:"));
+      process.exit(err);
+    }
+
+		//run post generation scripts
+
+  });
+
+};
+
+let args = minimist(process.argv.slice(2));
+require.main === module ? generate.apply(this, args._.slice(1)) : module.exports = generate;
