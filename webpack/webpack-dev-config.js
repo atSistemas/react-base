b1c4bb5dded9418597d2bb865ee39e23d417095a
Eduardo Pérez-Bermejo Galilea <eperez-bermejo@atsistemas.com>
2016-08-19 10:30:18 +0200

    refactor: unifies webpack configuration properties

--- webpack/webpack-dev-config.js
+++ webpack/webpack-dev-config.js
@@ -4,20 +4,19 @@ import ExtractTextPlugin from 'extract-text-webpack-plugin';
 import { symbols, color } from '../src/base/shared/console';
 
 const mainPath = path.resolve(__dirname, '..');
-const clientPath = path.resolve(__dirname, '..', 'src', 'base', 'client/');
+const clientPath = path.resolve(__dirname, '../src/base/client/');
 
 export const devTool = 'eval';
 
-export const devContext = path.resolve(__dirname, '..' ,'src' , 'app');
+export const context = path.resolve(__dirname, '..' ,'src' , 'app');
 
-export const devPlugins = [
+export const plugins = [
   new webpack.HotModuleReplacementPlugin(),
-  new webpack.optimize.OccurenceOrderPlugin(),
   new ExtractTextPlugin('bundle.css', { allChunks: true }),
   new webpack.DefinePlugin({'process.env': {'NODE_ENV': '"development"'}}),
 
   function() {
-    this.plugin("done", function(stats) {
+    this.plugin('done', function(stats) {
       if (stats.compilation.errors && stats.compilation.errors.length && process.argv.indexOf('--watch') == -1) {
         console.log('[BASE] ' + color('errpr', symbols.error) + stats.compilation.errors);
       }
@@ -25,25 +24,23 @@ export const devPlugins = [
   }
 ];
 
-export const devEntries = [
+export const entries = [
   clientPath,
   'webpack/hot/dev-server',
   'webpack-hot-middleware/client'
 ];
 
-export const devLoaders = [
-  { test: [/\.jsx?$/],loader: 'babel-loader',exclude: /node_modules/,include: mainPath, query: { presets: ["react-hmre", "es2015", "stage-0", "react"] }},
+export const loaders = [
+  { test: [/\.jsx?$/],loader: 'babel-loader',exclude: /node_modules/,include: mainPath, query: { presets: ['react-hmre', 'es2015', 'stage-0', 'react'] }},
   { test: /\.css$/, loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]-[hash:base64:4]!postcss-loader'}
 ];
 
-export const devPostCss = function (webpack) {
-  return [
+export const postCss = (webpack) =>
+  [
     require('postcss-import')({ addDependencyTo: webpack }),
-
     require('postcss-cssnext')(),
     require('postcss-modules-extract-imports'),
     require('postcss-nested')(),
     require('postcss-reporter')(),
     require('postcss-url')()
   ];
-};
--- webpack/webpack-prod-config.js
+++ webpack/webpack-prod-config.js
@@ -5,13 +5,13 @@ import ExtractTextPlugin from 'extract-text-webpack-plugin';
 import { symbols, color } from '../src/base/shared/console';
 
 const mainPath = path.resolve(__dirname, '..');
-const clientPath = path.resolve(__dirname, '..', 'src', 'base', 'client/');
+const clientPath = path.resolve(__dirname, '../src/base/client/');
 
-export const prodTool = 'cheap-module-source-map';
+export const devTool = 'cheap-module-source-map';
 
-export const prodContext = path.resolve(__dirname, '..', 'src', 'app');
+export const context = path.resolve(__dirname, '../src/app');
 
-export const prodPlugins = [
+export const plugins = [
   new webpack.NoErrorsPlugin(),
   new ExtractTextPlugin('bundle.css'),
   new webpack.optimize.DedupePlugin(),
@@ -21,7 +21,7 @@ export const prodPlugins = [
   new webpack.DefinePlugin({'process.env': {'NODE_ENV': '"production"'}}),
   new webpack.optimize.UglifyJsPlugin({compressor: { warnings: false }, output: {comments: false}}),
   function() {
-    this.plugin("done", function(stats) {
+    this.plugin('done', function(stats) {
       if (stats.compilation.errors && stats.compilation.errors.length && process.argv.indexOf('--watch') == -1) {
         console.log('[BASE] ' + color('error', symbols.error) + stats.compilation.errors);
       }
@@ -29,7 +29,7 @@ export const prodPlugins = [
   }
 ];
 
-export const prodEntries = {
+export const entries = {
   app: clientPath,
   common: [
     'react',
@@ -43,18 +43,17 @@ export const prodEntries = {
   ]
 };
 
-export const prodLoaders = [
-  { test: [/\.jsx?$/],loader: 'babel-loader',exclude: /node_modules/,include: mainPath, query: { presets: ["es2015", "stage-0", "react"] }},
+export const loaders = [
+  { test: [/\.jsx?$/],loader: 'babel-loader',exclude: /node_modules/, include: mainPath, query: { presets: ['es2015', 'stage-0', 'react'] }},
   { test: /\.css/, loader: ExtractTextPlugin.extract('style-loader',  'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]-[hash:base64:4]!postcss-loader')}
 ];
 
-export const prodPostCss = function (webpack) {
-  return [
-    require("postcss-import")({ addDependencyTo: webpack }),
+export const postCss = (webpack) =>
+  [
+    require('postcss-import')({ addDependencyTo: webpack }),
     require('postcss-modules-extract-imports'),
-    require("postcss-url")(),
-    require("postcss-cssnext")(),
-    require('cssnano')({autoprefixer: false}),
-    require("postcss-reporter")()
+    require('postcss-url')(),
+    require('postcss-cssnext')(),
+    require('postcss-reporter')(),
+    require('cssnano')({autoprefixer: false})
   ];
-};
