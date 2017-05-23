5fb063215b075fcadc002ec8f624aab7ad0385d1
Pablo Magaz <pmagaz@atsistemas.com>
2017-05-23 10:20:48 +0200

    Update destructuring

--- src/app/containers/Ecma6/modules/destructuring.js
+++ src/app/containers/Ecma6/modules/destructuring.js
@@ -1,18 +1,18 @@
 //Destructurating and object
 const obj = { name: 'Peter', surname: 'Weller' }; 
-const destructuredObj = { name } = obj;
+
+const { name, surname } = obj;
 
 //DESTRUCTURING PARAMS
 const arrowFunction = ({ name, surname }) => {
   return `hi ${name} ${surname}`;
 };
 
-export const destructured = arrowFunction(destructuredObj);
-
+export const destructured = arrowFunction(obj);
 
 //SAME AS
 const arrowFunction2 = props => {
   return `hi ${props.name} ${props.surname}`;
 };
 
-export const destructured2 = arrowFunction2(destructuredObj);
+export const destructured2 = arrowFunction2(obj);
