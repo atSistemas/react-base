531334c2437e62fad78d7da19cde57fe333c933f
Pablo Magaz <pmagaz@atsistemas.com>
2017-07-05 12:42:03 +0200

    Update to redux-req-middleware

--- server/middleware/routing-middleware.js
+++ server/middleware/routing-middleware.js
@@ -1,34 +1,28 @@
 import { match } from 'react-router';
+import { fetchRequiredActions } from 'base';
 
 import routes from '../../src/base/routes';
 import renderPage from '../lib/renderPage';
-import { fetchRequiredActions } from 'base';
 import renderContainer from '../lib/renderContainer';
 import configureServerStore from '../lib/configureStore';
 
-import { applyMiddleware, createStore } from 'redux';
-import rootReducer from '../../src/base/reducers/';
-import requestMiddleware from '../../src/base/middleware/Request';
-
-const context = 'server';
-
-export default function routingMiddleware(req, res, next) {
+export default function routingMiddleware(req, res) {
   
   const store = configureServerStore();
 
   match({ routes , location: req.url }, (error, redirectLocation, renderProps) => {
 
-    if ( error ) return res.status(500).send( error.message );
-    if ( redirectLocation ) return res.redirect( 302, redirectLocation.pathname + redirectLocation.search );
-    if ( renderProps == null ) return res.status(404).send( 'Not found' );
+    if (error) return res.status(500).send(error.message);
+    if (redirectLocation) return res.redirect(302, redirectLocation.pathname + redirectLocation.search);
+    if (renderProps == null) return res.status(404).send('Not found');
 
-    fetchRequiredActions(store.dispatch, renderProps.components, renderProps.params, context)
+    fetchRequiredActions(store.dispatch, renderProps.components, renderProps.params, 'server')
       .then(() => {
         const routeMatch = renderProps.location.pathname;
         const renderedContainer = renderContainer(store, renderProps);
-        return renderPage(routeMatch, renderedContainer, store );
+        return renderPage(routeMatch, renderedContainer, store);
       })
-      .then( page => res.status(200).send(page) )
-      .catch( err => res.end(err.message) );
+      .then(page => res.status(200).send(page))
+      .catch(err => res.end(err.message));
   });
 }
