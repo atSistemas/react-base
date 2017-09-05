0dc69ceb19f8f69307cbdf1101e825028f978bb3
Pablo Magaz <pmagaz@atsistemas.com>
2017-09-05 16:20:55 +0200

    Fix old template

--- src/base/conf/.react-base/templates/generator-react-base/generators/templates/_action.js
+++ src/base/conf/.react-base/templates/generator-react-base/generators/templates/_action.js
@@ -1,10 +1,9 @@
-import { generateFetchTypes } from 'base';
 import api from '../api';
 import ActionTypes from '../actionTypes';
 
 export function get<%= namePascal %>( { params } ) {
   return {
-    types: generateFetchTypes(ActionTypes.<%= nameUpper %>_REQUEST),
+    type: ActionTypes.<%= nameUpper %>_REQUEST,
     request: api.fetch<%= namePascal %>(params)
   };
 }
