dc2334e7e49defc908b59b99af61525248365926
Fernando Ramos <framos@atsistemas.com>
2017-11-28 13:29:59 +0100

    updates force of fetchRequiredActions

--- src/base/shared/FetchData.js
+++ src/base/shared/FetchData.js
@@ -1,25 +1,23 @@
-export function fetchRequiredActions(...args) {
+export const fetchRequiredActions = (...args) => {
   const serverContext = ~args.indexOf('server');
-  if (serverContext) {
-    return fetchServerData.apply(this, args);
-  } else {
-    return fetchClientData.apply(this, args);
-  }
-}
+  return serverContext ?
+    fetchServerData.apply(this, args)
+    :
+    fetchClientData.apply(this, args);
+};
 
-function fetchServerData(dispatch, components, params) {
-  const actions = components.reduce((prev, current) => {
-    return Object.keys(current).reduce((acc, key) => {
+const fetchServerData = (dispatch, components, params) => {
+  const actions = components.reduce((prev, current) => (
+    Object.keys(current).reduce((acc, key) => {
       const hasRequiredActions = current[key].hasOwnProperty('requiredActions');
       return hasRequiredActions ? current[key].requiredActions.concat(acc) : acc;
-    }, prev);
-  }, []);
+    }, prev)
+  ), []);
   const requiredActions = actions.map(action => dispatch(action(params)));
   return Promise.all(requiredActions);
-}
+};
 
-function fetchClientData(actions, props, node, force = false) {
-  const checkSize = props[node];
+const fetchClientData = (actions, props, force = false) => {
   const { params, dispatch } = props;
-  if (!checkSize || force) actions.map(action => dispatch(action(params)));
-}
+  force && actions.map(action => dispatch(action(params)));
+};
